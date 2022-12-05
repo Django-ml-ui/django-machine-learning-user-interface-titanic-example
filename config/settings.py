305a4ac0439c20c1b5a07b47ee9b25d7b74ab0f9
phikolomzi <ggwphi001@myuct.ac.za>
2022-12-05 05:29:01 +0200

    heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -134,3 +135,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MODELS = os.path.join(BASE_DIR, 'input/model')
+
