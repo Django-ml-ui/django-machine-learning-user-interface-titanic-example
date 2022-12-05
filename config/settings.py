796227967613e4a3ad641fa7f8d1ae93ddaf2bab
phikolomzi <ggwphi001@myuct.ac.za>
2022-12-05 05:33:48 +0200

    heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY='l($-5ppvbxy#tri^w&669+=i6^xxls!z=2o$!cbi79zj4)c8^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'django-ml-ui-titanic.herokuapp.com']
 
 
 # Application definition
